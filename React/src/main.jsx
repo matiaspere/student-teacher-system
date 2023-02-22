b87a89f7c3bebaa532536bfa4932669190c92c81
Matias Peressutti <matiperessutti@gmail.com>
2023-02-22 18:10:15 -0300

    add router and context

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { ContextProvider } from "../context/ContextProvider";
+import router from "./router";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <ContextProvider>
+            <RouterProvider router={router} />
+        </ContextProvider>
+    </React.StrictMode>
+);
---
+++ React/src/router.jsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter, Navigate} from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Navigate to="/home" />,
+            },
+            {
+                path: "/home",
+                element: <Home />,
+            },
+            {
+                path: "/listing",
+                element: <Listing />,
+            },
+        ],
+    },
+    {
+        path: "/",
+        element: <GuestLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+                element: <Signup />,
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: <NotFound />,
+    },
+]);
+
+export default router;
